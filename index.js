97287a9980ebbc1231f460b6578583c5d6932d33
AL Riyad <imalriyad@gmail.com>
2024-02-02 01:31:36 +0600

    message broadcasting with socket room added

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const http = require("http");
 const serverio = require("socket.io");
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+// Middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
 const server = http.createServer(app);
 const io = serverio(server, {
+  allowEIO3: true,
   cors: {
-    origin: "https://swiftchatx.netlify.app",
-    methods: ["GET", "POST"],
+    origin: "http://localhost:5173",
+    credentials: true, // Allow credentials (cookies)
   },
 });
 
@@ -43,58 +54,107 @@ async function run() {
     const userCollection = db.collection("users");
 
     io.on("connection", (socket) => {
-      socket.on("connection", (userName) => {
-        socket.broadcast.emit(
-          "connection",
-          `${userName?.name} has joined the chat`
-        );
+      socket.on("joinRoom", (roomId) => {
+        socket.join(roomId);
+        socket.room = roomId;
       });
 
+      // Send a message
       socket.on("sendMessage", (data) => {
-        socket.broadcast.emit("message", data);
-
-        // Storing messages to mongodb
-        app.post("/api/v1/save-message", async (req, res) => {
-          const data = req.body;
-          const conversationId = createConversationId(
-            data.senderEmail,
-            data.receiverEmail
-          );
-          const result = await conversationsCollection.updateOne(
-            { _id: conversationId },
-            { $push: { messages: data } },
-            { upsert: true }
-          );
-          res.send(result);
-        });
+        socket.to(socket.room).emit("message", data);
       });
 
-      const createConversationId = (user1, user2) => {
-        const sortedEmails = [user1, user2].sort();
-        return sortedEmails.join("-");
-      };
+      // Handle typing events
+      socket.on("typing", (data) => {
+        const typingData = { ...data, room: socket.room };
+        // Broadcast the typing notification to everyone in the room except the sender
+        socket.to(socket.room).emit("typingNotify", typingData);
+      });
 
       socket.on("disconnect", () => {
-        console.log("A user disconnected");
-      });
-
-      socket.on("typing", (data) => {
-        socket.broadcast.emit("typing", data);
+        console.log(`User disconnected: ${socket.id}`);
+        socket.leaveAll();
       });
     });
+
+    // Storing messages to mongodb
+    app.post("/api/v1/save-message", async (req, res) => {
+      const data = req.body;
+      const conversationId = createConversationId(
+        data.senderEmail,
+        data.receiverEmail
+      );
+      const result = await conversationsCollection.updateOne(
+        { _id: conversationId },
+        { $push: { messages: data } },
+        { upsert: true }
+      );
+      res.send(result);
+    });
+
+    const createConversationId = (user1, user2) => {
+      const sortedEmails = [user1, user2].sort();
+      return sortedEmails.join("-");
+    };
 
     // Get messages for two users conversation
     app.get("/api/v1/get-messages", async (req, res) => {
       const searchingID = req?.query?.id;
-      console.log(searchingID);
       const query = { _id: searchingID };
       const result = await conversationsCollection.find(query).toArray();
       res.send(result);
     });
 
+    // creating jwt token on login
+    app.post("/api/v1/jwt-token", (req, res) => {
+      const userEmail = req?.body?.email;
+      const token = jwt.sign({ email: userEmail }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+      });
+
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+
+        .send({ success: true });
+    });
+
+    // Removing cookie when user logout
+    app.post("/api/v1/logout", async (req, res) => {
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          secure: process.env.NODE_ENV === "production" ? true : false,
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ status: true });
+    });
+
+    // get user by searching
+    app.get("/api/v1/user", async (req, res) => {
+      const searchQuery = req.query?.name;
+      let filter;
+      if (searchQuery) {
+        filter = {
+          name: { $regex: new RegExp(searchQuery, "i") },
+        };
+      }
+      const options = {
+        projection: { password: 0 },
+      };
+      const result = await userCollection.find(filter, options).toArray();
+      res.send(result);
+    });
+
     // api for getting user from db
     app.get("/api/v1/get-user", async (req, res) => {
-      const result = await userCollection.find().toArray();
+      const options = {
+        projection: { password: 0 },
+      };
+      const result = await userCollection.find({}, options).toArray();
       res.send(result);
     });
 
